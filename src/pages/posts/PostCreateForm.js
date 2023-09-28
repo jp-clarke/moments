99545d0bb090ef0cab96af80f2cc8e5c0df44dcb
John Clarke <jp_clarke@outlook.com>
2023-09-28 21:08:02 +0000

    Add user redirect functionality

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -15,8 +15,10 @@ import Asset from "../../components/Asset.js";
 import { Alert, Image } from "react-bootstrap";
 import { useHistory } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import { useRedirect } from "../../hooks/useRedirect";
 
 function PostCreateForm() {
+  useRedirect("loggedOut");
   const [errors, setErrors] = useState({});
 
   const [postData, setPostData] = useState({
